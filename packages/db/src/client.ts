5e76ed464efde08775a73c1957f25891178d9d32
Maksim Karpychev <kodermax@gmail.com>
2024-10-01 09:51:37 +0300

    🐛 (client.ts): fix potential runtime error by providing a fallback for POSTGRES_URL environment variable

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.POSTGRES_URL!);
+const sql = neon(process.env.POSTGRES_URL ?? "");
 
 export const db = drizzle(sql, { schema });
