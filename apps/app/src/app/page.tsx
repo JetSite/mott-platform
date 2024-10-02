487f74ca56dfc4c2e3aba4aa6eebb6ad8a22ec7d
Maksim <kodermax@gmail.com>
2024-10-02 19:30:59 +0300

    ✨ (globals.css): update primary color variables for a more accessible design ✨ (globals.css): add chart color variables for improved data visualization ✨ (globals.css): apply new global styles and container adjustments ♻️ (page.tsx): remove post-related components and simplify HomePage

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,12 +1,5 @@
-import { Suspense } from "react";
-
 import { HydrateClient } from "~/trpc/server";
 import { AuthShowcase } from "../components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "../components/posts";
 
 export default function HomePage() {
   return (
@@ -14,24 +7,9 @@ export default function HomePage() {
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-primary">T3</span> Turbo
+            Welcome to <span className="text-primary">Mott.ai</span>
           </h1>
           <AuthShowcase />
-
-          <CreatePostForm />
-          <div className="w-full max-w-2xl overflow-y-scroll">
-            <Suspense
-              fallback={
-                <div className="flex w-full flex-col gap-4">
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                </div>
-              }
-            >
-              <PostList />
-            </Suspense>
-          </div>
         </div>
       </main>
     </HydrateClient>
