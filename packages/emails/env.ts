3f5113ab20f24b7810539f778d259d44472ce7d4
Maksim <kodermax@gmail.com>
2024-10-02 17:06:30 +0300

    ♻️ (env.ts): rename MAILDEV_* variables to EMAIL_SANDBOX_* for clarity

--- packages/emails/env.ts
+++ packages/emails/env.ts
@@ -4,12 +4,12 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     RESEND_API_KEY: z.string().optional(),
-    MAILDEV_ENABLED: z.string().optional(),
-    MAILDEV_HOST: z.string().optional(),
+    EMAIL_SANDBOX_ENABLED: z.string().optional(),
+    EMAIL_SANDBOX_HOST: z.string().optional(),
   },
   runtimeEnv: {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
-    MAILDEV_ENABLED: process.env.MAILDEV_ENABLED,
-    MAILDEV_HOST: process.env.MAILDEV_HOST,
+    EMAIL_SANDBOX_ENABLED: process.env.EMAIL_SANDBOX_ENABLED,
+    EMAIL_SANDBOX_HOST: process.env.EMAIL_SANDBOX_HOST,
   },
 });
