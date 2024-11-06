95d3c48c76a04116eab77d48910b16dbe3963154
Maksim <kodermax@gmail.com>
2024-11-06 18:10:54 +0300

    ♻️ (workspace.ts): remove unused protectedProcedure to clean up code and improve maintainability

--- packages/api/src/router/workspace.ts
+++ packages/api/src/router/workspace.ts
@@ -1,9 +1,3 @@
 import type { TRPCRouterRecord } from "@trpc/server";
 
-import { protectedProcedure } from "../trpc";
-
-export const workspaceRouter = {
-  get: protectedProcedure.query(async ({ ctx }) => {
-    return null;
-  }),
-} satisfies TRPCRouterRecord;
+export const workspaceRouter = {} satisfies TRPCRouterRecord;
